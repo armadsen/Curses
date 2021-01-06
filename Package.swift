ac41ed8341b7b44cfe983ddcb216c05d7461d141
Andrew Madsen <andrew@openreelsoftware.com>
2021-01-05 17:13:49 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
              targets: ["Curses"]),
   ], 
   dependencies: [
-    .package(url:  "https://github.com/armadsen/CNCURSES.git", from: "1.0.0"),
+    .package(url:  "https://github.com/armadsen/CNCURSES.git", from: "1.0.3"),
   ],
   targets: [
     .target(
